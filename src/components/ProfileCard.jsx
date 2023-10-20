46f6987b9a52ab782f0f342bdc88d2589a1e8893
shivamrai27 <200shivamrai@gmail.com>
2023-10-20 23:30:36 +0530

    Add Employee

---
+++ src/components/ProfileCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+const ProfileCard = () => {
+    return (
+        <Container>
+            <h1>Hello</h1>
+        </Container>
+    )
+}
+
+export default ProfileCard
+
+const Container = styled.div`
+background-color: red;
+`
