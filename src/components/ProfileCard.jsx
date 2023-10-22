2eb2832aa321e250a3449962d631faa6b18d61d2
shivamrai27 <200shivamrai@gmail.com>
2023-10-23 03:44:59 +0530

    Add Employee

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -3,7 +3,16 @@ import styled from 'styled-components'
 const ProfileCard = () => {
     return (
         <Container>
-            <h1>Hello</h1>
+            <div className='ProfileCard'>
+                <div>
+                    <img src={"./public/images/3.jpg"} />
+                </div>
+                <div>John Doe</div>
+                <div className='location'>Delhi</div>
+            </div>
+
+
+
         </Container>
     )
 }
@@ -11,5 +20,25 @@ const ProfileCard = () => {
 export default ProfileCard
 
 const Container = styled.div`
-background-color: red;
+    display: flex;
+    margin-top: 10px;
+    justify-content: center;
+    /* align-items: center; */
+    gap: 9px;
+    
+    .ProfileCard{
+     border: 2px solid red;
+     width: 300px;
+     height: 350px;
+     text-align: center;
+    }
+    img{
+        border-radius: 50%;
+        border: 3px solid black;
+        display: flex;
+        justify-content: center;
+        margin: auto;
+        margin-top: 10px;
+    }
+;
 `
